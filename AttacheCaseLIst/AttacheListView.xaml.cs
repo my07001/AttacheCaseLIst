df650d3ea01758471bc420b28a1dd27fc2fad586
masa <my07001@gmail.com>
2026-02-27 13:08:04 +0900

    Initial commit

---
+++ AttacheCaseLIst/AttacheListView.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace WpfTutorialSamples.ListView_control
+{
+    public partial class ListViewColumnSortingSample : Window
+    {
+        private GridViewColumnHeader listViewSortCol = null;
+        private SortAdorner listViewSortAdorner = null;
+
+        public class AttacheFile
+        {
+            public string name { get; set; } = string.Empty;
+
+            public string lastWriteTime { get; set; } = string.Empty;
+        }
+
+        public ListViewColumnSortingSample()
+        {
+            InitializeComponent();
+            string[] getFiles = System.IO.Directory.GetFiles(@"C:\_User\atc", "*.atc");
+            attaItems.ItemsSource = makeListAttacheFiles(getFiles).OrderByDescending(x => x.lastWriteTime);
+            itemCount.Content = attaItems.Items.Count + " items";
+        }
+
+        private AttacheFile makeListAttache(string item)
+        {
+            var filedata = new AttacheFile();
+            filedata.name = item;
+            filedata.lastWriteTime = File.GetLastWriteTime(item).ToString();
+ 
+            return filedata;
+        }
+
+        private List<AttacheFile> makeListAttacheFiles(string[] getFiles)
+        {
+            List<AttacheFile> attacheFiles = new();
+
+            foreach (string item in getFiles)
+            {
+                attacheFiles.Add(makeListAttache(item));
+            }
+
+            return attacheFiles;
+        }
+
+        private List<AttacheFile> makeListAttacheFiles(List<string> getFiles)
+        {
+            List<AttacheFile> attacheFiles = new();
+
+            foreach (string item in getFiles)
+            {
+                attacheFiles.Add(makeListAttache(item));
+            }
+
+            return attacheFiles;
+        }
+
+        private void attaItemsColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+            GridViewColumnHeader column = (sender as GridViewColumnHeader);
+            string sortBy = column.Tag.ToString();
+            if (listViewSortCol != null)
+            {
+                AdornerLayer.GetAdornerLayer(listViewSortCol).Remove(listViewSortAdorner);
+                attaItems.Items.SortDescriptions.Clear();
+            }
+
+            ListSortDirection newDir = ListSortDirection.Ascending;
+            if (listViewSortCol == column && listViewSortAdorner.Direction == newDir)
+                newDir = ListSortDirection.Descending;
+
+            listViewSortCol = column;
+            listViewSortAdorner = new SortAdorner(listViewSortCol, newDir);
+            AdornerLayer.GetAdornerLayer(listViewSortCol).Add(listViewSortAdorner);
+            attaItems.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
+        }
+
+        private void SearchTextBox_ChangedEventHandler(object sender, TextChangedEventArgs args)
+        {
+            if (args.Changes.Count > 0)
+            {
+                var textbox = sender as TextBox;
+                //System.Diagnostics.Debug.WriteLine(textbox);
+
+                List<string> getFiles = System.IO.Directory.GetFiles(@"C:\_User\atc", "*.atc").Where(x => x.Contains(textbox.Text)).ToList();
+                attaItems.ItemsSource = makeListAttacheFiles(getFiles);
+                itemCount.Content = attaItems.Items.Count + " items";
+            }
+        }
+
+        private void ListViewItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            // sender がダブルクリックされた項目
+            ListView targetItem = (ListView)sender;
+
+            ProcessStartInfo pInfo = new ProcessStartInfo();
+            pInfo.UseShellExecute = true;
+            pInfo.FileName = "C:\\Program Files (x86)\\AttacheCase3\\AttacheCase.exe";
+
+            AttacheFile ta = (AttacheFile)targetItem.SelectedValue;
+            pInfo.Arguments = '\"' + ta.name + '\"';
+
+            Process.Start(pInfo);
+        }
+    }
+
+    public class SortAdorner : Adorner
+    {
+        private static Geometry ascGeometry =
+            Geometry.Parse("M 0 4 L 3.5 0 L 7 4 Z");
+
+        private static Geometry descGeometry =
+            Geometry.Parse("M 0 0 L 3.5 4 L 7 0 Z");
+
+        public ListSortDirection Direction { get; private set; }
+
+        public SortAdorner(UIElement element, ListSortDirection dir)
+            : base(element)
+        {
+            this.Direction = dir;
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+            if (AdornedElement.RenderSize.Width < 20)
+                return;
+
+            TranslateTransform transform = new TranslateTransform
+                (
+                    AdornedElement.RenderSize.Width - 15,
+                    (AdornedElement.RenderSize.Height - 5) / 2
+                );
+            drawingContext.PushTransform(transform);
+
+            Geometry geometry = ascGeometry;
+            if (this.Direction == ListSortDirection.Descending)
+                geometry = descGeometry;
+            drawingContext.DrawGeometry(Brushes.Black, null, geometry);
+
+            drawingContext.Pop();
+        }
+    }
+}
---
+++ AttacheCaseLIst/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfTutorialSamples.ListView_control;
+
+namespace AttacheCaseLIst
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<string> FileList;
+
+        public class AttacheFile
+        {
+            public string name { get; set; } = string.Empty;
+
+            public string lastWriteTime { get; set; } = string.Empty;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FileList = new ObservableCollection<string>();
+            //FileListBox.ItemsSource = FileList;
+            FileListBox.ItemsSource = System.IO.Directory.GetFiles(@"C:\_User\atc", "*.atc");
+
+
+            List<AttacheFile> attacheFiles = new();
+            string[] getFiles = System.IO.Directory.GetFiles(@"C:\_User\atc", "*.atc");
+            foreach (string item in getFiles)
+            {
+                var filedata = new AttacheFile();
+                filedata.name = item;
+                filedata.lastWriteTime = File.GetLastWriteTime(item).ToString();
+                attacheFiles.Add(filedata);
+            }
+
+
+        }
+
+        private void FileListBox_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                var fileNames = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (var name in fileNames)
+                {
+                    FileList.Add(name);
+                }
+            }
+        }
+
+        private void FileListBox_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+            e.Handled = true;
+        }
+
+        private void FileListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var listBoxItem = sender as ListBoxItem;
+
+            ProcessStartInfo pInfo = new ProcessStartInfo();
+            pInfo.UseShellExecute = true;
+            pInfo.FileName = "C:\\Program Files (x86)\\AttacheCase3\\AttacheCase.exe";
+
+            var path1 = (string)FileListBox.Items[FileListBox.SelectedIndex];
+            //System.Diagnostics.Debug.WriteLine(path1);
+
+            pInfo.Arguments = path1;
+
+            Process.Start(pInfo);
+        }
+
+        private void SearchTextBox_ChangedEventHandler(object sender, TextChangedEventArgs args)
+        {
+            if(args.Changes.Count > 0)
+            {
+                var textbox = sender as TextBox;
+                //System.Diagnostics.Debug.WriteLine(textbox);
+
+                FileList = new ObservableCollection<string>();
+                FileListBox.ItemsSource = System.IO.Directory.GetFiles(@"C:\_User\atc", "*.atc").Where(x => x.Contains(textbox.Text)).ToList();
+            }
+        }
+    }
+}
